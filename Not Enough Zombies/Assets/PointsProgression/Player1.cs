b984f16c5396e3e83c652e7de7b271cde5ee348a
kehindetinubu <kehindetinubu11@gmail.com>
2022-05-20 11:19:26 +0100

    errors

---
+++ Not Enough Zombies/Assets/PointsProgression/Player1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+
+	public int maxHealth = 100;
+	public int currentHealth;
+
+	public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (currentHealth > maxHealth)
+        {
+			currentHealth = maxHealth;
+        }
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("Damaged");
+			TakeDamage(20);
+		}
+
+		if (Input.GetButtonDown("Fire1"))
+        {
+			if (currentHealth < maxHealth)
+            {
+				Debug.Log("Healed");
+				TakeHeal(Random.Range(5,10));
+            }
+        }
+    }
+
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
+
+	void TakeHeal(int heal)
+    {
+		currentHealth += heal;
+
+		healthBar.SetHealth(currentHealth);
+    }
+}
---
+++ Not Enough Zombies/Assets/PointsProgression/Target.cs
@@ -0,0 +1,54 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Target : MonoBehaviour
+{
+
+    public int maxHealth = 150;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    public GameObject FloatingTextPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage (int amount)
+    {
+        currentHealth -= amount;
+        healthBar.SetHealth(currentHealth); 
+
+        //trigger floating text here
+        if (FloatingTextPrefab)
+        {
+            ShowFloatingText();
+        }
+
+        Debug.Log(gameObject.name + " health = " + currentHealth);
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void ShowFloatingText()
+    {
+        var go = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+        go.GetComponent<TextMeshPro>().text = currentHealth.ToString();
+        go.GetComponent<TextMeshPro>().faceColor = healthBar.fill.color;
+    }
+
+
+    void Die ()
+    {
+        ScoringSystem.points += 50;
+        Destroy(gameObject);
+    }
+    
+}
